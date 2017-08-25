7c8fb32aae194798709baa0be85aec9567260c78
songx <928277484@qq.com>
2017-08-25 10:14:04 +0800

    import class

--- src/main/java/org/seckill/config/applicationContextConfig.java
+++ src/main/java/org/seckill/config/applicationContextConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.security.access.SecurityConfig;
 
 @Configuration
 @Import({SpringDataConfig.class, SpringRedisConfig.class, SecurityConfig.class})
